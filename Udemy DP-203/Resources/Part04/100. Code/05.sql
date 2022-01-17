8e0829e84c66a07c7698e87c60296cc7e0f3040e
Denis Zangarov <denisimo_98@yahoo.com>
2022-01-17 17:54:46 +0200

    Update 05.sql

--- Udemy DP-203/Resources/Part04/100. Code/05.sql
+++ Udemy DP-203/Resources/Part04/100. Code/05.sql
@@ -1,36 +1,30 @@
 -- Lab - Loading data into a table - COPY Command - Parquet
 
-DROP TABLE [logdata]
 
 -- Recreate the table
 -- Here again I am using the data type with MAX because that is how I generated the parquet files when it came to the data type
 -- Here we need to specify the clustered index based on a column , because indexes can't be created on varchar(MAX)
 
-CREATE TABLE [logdata]
+CREATE TABLE [logdata_parquet_copy]
 (
-        [Id] [int] NULL,
-	[Correlationid] [varchar](200) NULL,
-	[Operationname] [varchar](200) NULL,
-	[Status] [varchar](100) NULL,
-	[Eventcategory] [varchar](100) NULL,
-	[Level] [varchar](100) NULL,
-	[Time] [datetime] NULL,
-	[Subscription] [varchar](200) NULL,
-	[Eventinitiatedby] [varchar](1000) NULL,
-	[Resourcetype] [varchar](1000) NULL,
-	[Resourcegroup] [varchar](1000) NULL
+    [Id] [int],
+	[Correlationid] [varchar](200),
+	[Operationname] [varchar](200),
+	[Status] [varchar](100),
+	[Eventcategory] [varchar](100),
+	[Level] [varchar](100),
+	[Time] [datetime],
+	[Subscription] [varchar](200),
+	[Eventinitiatedby] [varchar](1000),
+	[Resourcetype] [varchar](1000),
+	[Resourcegroup] [varchar](1000)
 )
 
--- Grant the permissions again. Because when you drop the table, you also drop the associated permission
-
-GRANT INSERT ON logdata TO user_load;
-GRANT SELECT ON logdata TO user_load;
-
-COPY INTO [logdata] FROM 'https://appdatalake7000.blob.core.windows.net/data/parquet/*.parquet'
+COPY INTO [logdata_parquet_copy] FROM 'https://h24pgen2.blob.core.windows.net/data/raw/parquet/*.parquet'
 WITH
 (
 FILE_TYPE='PARQUET',
-CREDENTIAL=(IDENTITY= 'Shared Access Signature', SECRET='sv=2020-02-10&ss=b&srt=sco&sp=rl&se=2021-07-01T16:07:07Z&st=2021-07-01T08:07:07Z&spr=https&sig=j%2BtdThwbGU83Ol3LyyLHbFZQTMyGauCVtfKbUuUCkLM%3D')
+CREDENTIAL=(IDENTITY= 'Shared Access Signature', SECRET='sv=2020-08-04&ss=bfqt&srt=sco&sp=rwdlacupx&se=2022-01-17T23:27:27Z&st=2022-01-17T15:27:27Z&spr=https&sig=DDvOc65xuDbh%2BFsc94pmPJWw5e3KbqkLpq8hGy5huDw%3D')
 )
 
-SELECT * FROM [logdata]
+SELECT * FROM [logdata_parquet_copy]
