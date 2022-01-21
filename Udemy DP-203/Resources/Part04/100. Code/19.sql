4440c658828fe37fdeb64155cc17d4c0c65014fc
Denis Zangarov <denisimo_98@yahoo.com>
2022-01-21 09:50:02 +0200

    part 4 ready

--- Udemy DP-203/Resources/Part04/100. Code/19.sql
+++ Udemy DP-203/Resources/Part04/100. Code/19.sql
@@ -1,39 +1,7 @@
 -- Lab - Creating a table with partitions
+DROP TABLE [logdata_new]
 
-DROP TABLE [logdata]
-
-CREATE TABLE [logdata]
-(
-    [Id] [int] NULL,
-	[Correlationid] [varchar](200) NULL,
-	[Operationname] [varchar](200) NULL,
-	[Status] [varchar](100) NULL,
-	[Eventcategory] [varchar](100) NULL,
-	[Level] [varchar](100) NULL,
-	[Time] [datetime] NULL,
-	[Subscription] [varchar](200) NULL,
-	[Eventinitiatedby] [varchar](1000) NULL,
-	[Resourcetype] [varchar](1000) NULL,
-	[Resourcegroup] [varchar](1000) NULL
-)
-
-COPY INTO logdata FROM 'https://datalake2000.blob.core.windows.net/data/cleaned/Log.csv'
-WITH
-(
-FIRSTROW=2
-)
-
--- Let's first inspect our table to see the range of dates
-
-SELECT FORMAT(Time,'yyyy-MM-dd') AS dt,COUNT(*) FROM logdata
-GROUP BY FORMAT(Time,'yyyy-MM-dd')
-
--- Let's drop the existing table if its exists
-DROP TABLE logdata
-
-
--- Let's create a new table with partitions
-CREATE TABLE [logdata]
+CREATE TABLE [logdata_new]
 (
     [Id] [int] NULL,
 	[Correlationid] [varchar](200) NULL,
@@ -55,12 +23,13 @@ PARTITION ( [Time] RANGE RIGHT FOR VALUES
    )  
 )
 
--- Copy data into the table
-COPY INTO logdata FROM 'https://datalake2000.blob.core.windows.net/data/cleaned/Log.csv'
+COPY INTO logdata_new FROM 'https://h24pgen2.blob.core.windows.net/data/raw/Log.csv'
 WITH
 (
 FIRSTROW=2
 )
+DROP TABLE [logdata_new]
+
 
 -- View the partitions
 SELECT  QUOTENAME(s.[name])+'.'+QUOTENAME(t.[name]) as Table_name
@@ -73,7 +42,8 @@ JOIN    sys.tables     t    ON    p.[object_id]   = t.[object_id]
 JOIN    sys.schemas    s    ON    t.[schema_id]   = s.[schema_id]
 JOIN    sys.indexes    i    ON    p.[object_id]   = i.[object_Id]
                             AND   p.[index_Id]    = i.[index_Id]
-WHERE t.[name] = 'logdata'
+WHERE t.[name] = 'logdata_new'
+
 
 SELECT o.name, pnp.index_id, pnp.partition_id, pnp.rows,   
     pnp.data_compression_desc, pnp.pdw_node_id  
@@ -86,6 +56,5 @@ JOIN sys.pdw_table_mappings AS TMap
     AND substring(TMap.physical_name,40, 10) = pnp.distribution_id 
 JOIN sys.objects AS o  
     ON TMap.object_id = o.object_id  
-WHERE o.name = 'logdata'  
+WHERE o.name = 'logdata_new'  
 ORDER BY o.name, pnp.index_id, pnp.partition_id;  
-
